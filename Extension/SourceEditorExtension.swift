2caa86640d474ce2cda739c751a32366b0fca69b
Yoshitaka Seki <takassekiyoshi@gmail.com>
2017-09-07 19:58:25 +0900

    :+1: Fix the extension name and command names

--- Extension/SourceEditorExtension.swift
+++ Extension/SourceEditorExtension.swift
@@ -11,17 +11,24 @@ import XcodeKit
 
 class SourceEditorExtension: NSObject, XCSourceEditorExtension {
     
-    /*
     func extensionDidFinishLaunching() {
         // If your extension needs to do any work at launch, implement this optional method.
+        print("launched!")
     }
-    */
-    
-    /*
+
     var commandDefinitions: [[XCSourceEditorCommandDefinitionKey: Any]] {
         // If your extension needs to return a collection of command definitions that differs from those in its Info.plist, implement this optional property getter.
-        return []
+
+        print("definition requested")
+
+        let bundleIdentifiler = Bundle.main.bundleIdentifier!
+        return [
+            [
+                .nameKey: "commandName",
+                .classNameKey: SourceEditorCommand.className(),
+                .identifierKey: bundleIdentifiler + "." + "commandName"
+            ]
+        ]
     }
-    */
-    
+
 }
