f642069cdd468f3e8c09156f3c2889fa5d18fcb3
Yoshitaka Seki <takassekiyoshi@gmail.com>
2017-09-07 19:24:34 +0900

    Initial Commit

---
+++ XcodeExtensionSample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  XcodeExtensionSample
+//
+//  Created by Yoshitaka Seki on 2017/09/07.
+//  Copyright © 2017年 takasek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ XcodeExtensionSample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XcodeExtensionSample
+//
+//  Created by Yoshitaka Seki on 2017/09/07.
+//  Copyright © 2017年 takasek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
